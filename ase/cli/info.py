5d5ded5580992d8b944fe75b0bcefd026ad720a2
Ask Hjorth Larsen <asklarsen@gmail.com>
2024-05-31 12:31:29 +0200

    Update and clean up "ase info" command.
     * Remove old calculator listings which will not work
     * ase info --calculators now lists the configuration as in ase.codes
     * Require ase info --files [NAME] to list files.  Previously all names
       were automatically taken to be filenames, which would prevent
       other options from making good use of positional arguments, or at
       least make it confusing
     * Split function into smaller functions

--- ase/cli/info.py
+++ ase/cli/info.py
@@ -6,84 +6,75 @@
 class CLICommand:
     """Print information about files or system.
 
-    Without any filename(s), informations about the ASE installation will be
-    shown (Python version, library versions, ...).
-
-    With filename(s), the file format will be determined for each file.
+    Without arguments, show information about ASE installation
+    and library versions of dependencies.
     """
 
     @staticmethod
     def add_arguments(parser):
-        parser.add_argument('filename', nargs='*',
-                            help='Name of file to determine format for.')
+        parser.add_argument('--files', nargs='*', metavar='PATH',
+                            help='Print information about specified files.')
         parser.add_argument('-v', '--verbose', action='store_true',
-                            help='Show more information about files.')
+                            help='Show additional information about files.')
         parser.add_argument('--formats', action='store_true',
                             help='List file formats known to ASE.')
-        parser.add_argument('--config', action='store_true',
-                            help='List configured calculators')
-        parser.add_argument('--calculators', action='store_true',
+        parser.add_argument('--calculators', nargs='*', metavar='NAME',
                             help='List all calculators known to ASE '
-                            'and whether/how each is installed.  Also, '
-                            'attempt to determine version numbers by '
-                            'running binaries or importing packages as '
-                            'appropriate.')
+                            'and their configuration. Specify names of '
+                            'calculators to show only those.')
 
     @staticmethod
     def run(args):
-        from ase.config import cfg
-        from ase.io.bundletrajectory import print_bundletrajectory_info
-        from ase.io.formats import UnknownFileTypeError, filetype, ioformats
-        from ase.io.ulm import print_ulm_info
-        if not args.filename:
-            print_info()
-            if args.formats:
-                print()
-                print_formats()
-            if args.config:
-                print()
-                cfg.print_everything()
+        if args.calculators is not None:
+            from ase.codes import list_codes, codes
             if args.calculators:
-                print()
-                cfg.check_calculators()
-                # print()
-                # from ase.calculators.autodetect import (detect_calculators,
-                #                                        format_configs)
-                # configs = detect_calculators()
-                # print('Calculators:')
-                # for message in format_configs(configs):
-                #     print('  {}'.format(message))
-                # print()
-                # print('Available: {}'.format(','.join(sorted(configs))))
+                names = args.calculators
+            else:
+                names = [*codes]
+            list_codes(names)
             return
 
-        n = max(len(filename) for filename in args.filename) + 2
-        nfiles_not_found = 0
-        for filename in args.filename:
-            try:
-                format = filetype(filename)
-            except FileNotFoundError:
-                format = '?'
-                description = 'No such file'
-                nfiles_not_found += 1
-            except UnknownFileTypeError:
-                format = '?'
-                description = '?'
+        if args.files:
+            print_file_info(args)
+            return
+
+        print_info()
+        if args.formats:
+            print()
+            print_formats()
+
+
+def print_file_info(args):
+    from ase.io.formats import UnknownFileTypeError, filetype, ioformats
+    from ase.io.bundletrajectory import print_bundletrajectory_info
+    from ase.io.ulm import print_ulm_info
+    n = max(len(filename) for filename in args.files) + 2
+    nfiles_not_found = 0
+    for filename in args.files:
+        try:
+            format = filetype(filename)
+        except FileNotFoundError:
+            format = '?'
+            description = 'No such file'
+            nfiles_not_found += 1
+        except UnknownFileTypeError:
+            format = '?'
+            description = '?'
+        else:
+            if format in ioformats:
+                description = ioformats[format].description
             else:
-                if format in ioformats:
-                    description = ioformats[format].description
-                else:
-                    description = '?'
+                description = '?'
 
-            print('{:{}}{} ({})'.format(filename + ':', n,
-                                        description, format))
-            if args.verbose:
-                if format == 'traj':
-                    print_ulm_info(filename)
-                elif format == 'bundletrajectory':
-                    print_bundletrajectory_info(filename)
+        print('{:{}}{} ({})'.format(filename + ':', n,
+                                    description, format))
+        if args.verbose:
+            if format == 'traj':
+                print_ulm_info(filename)
+            elif format == 'bundletrajectory':
+                print_bundletrajectory_info(filename)
 
-        raise SystemExit(nfiles_not_found)
+    raise SystemExit(nfiles_not_found)
 
 
 def print_info():
